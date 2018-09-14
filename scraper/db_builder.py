f87fd86d24a4480e0065c3ac4a51e1aa8cfcc361
Clara Pouletty <cpouletty@gmail.com>
2018-09-14 11:39:04 +0100

    applied fixes to modify_table, merged with original db_builder script

--- scraper/db_builder.py
+++ scraper/db_builder.py
@@ -1,18 +1,16 @@
-from psycopg2 import sql
+# -*- coding: utf-8 -*-
 import psycopg2
+from psycopg2 import sql
 import pandas as pd
 import glob
 from sqlalchemy import create_engine
 import sys
 
-READ_PATH, UN, PW = "", "", ""
-H, DB, PORT = "", "", ""
 MODE = "fail" #or "append"
 try:
-    from settings import *
+    import settings
 except ImportError as exc:
-    sys.stderr.write("Error: failed to import module ({})".format(exc))
-    pass
+    sys.exit(f"Error: failed to import module ({exc})")
 
 
 def upload_new_table(cursor, connection, engine, df, table_name):
@@ -20,7 +18,7 @@ def upload_new_table(cursor, connection, engine, df, table_name):
 
     old, new = sql.Identifier(table_name + "_old"), sql.Identifier(table_name)
 
-    cursor.execute(sql.SQL("DROP TABLE IF EXISTS {};").format(new))
+    cursor.execute(sql.SQL("DROP TABLE IF EXISTS {};").format(old))
     cursor.execute(sql.SQL("ALTER TABLE IF EXISTS {} RENAME TO {};").format(new, old))
     connection.commit()
     print(f"Removed old {table_name} table and renamed current to old.")
@@ -31,16 +29,44 @@ def upload_new_table(cursor, connection, engine, df, table_name):
         print(f"Populated {table_name} table.")
     except ValueError:
         print(f"Could not populate {table_name} table, as it already exists")
-        pass
+        exit()
+
+
+def modify_table(cursor, connection, engine, df, table_name):
+    # Grab new table and finding maximum line_id in previously existing table
+    current_table = sql.Identifier(table_name)
+    cursor.execute(sql.SQL(f"SELECT MAX(line_id) AS max_index FROM {current_table};"))
+    max_result = cursor.fetchall()
+
+    # Create row counter column for new data frame beginning from the largest line_id of the existing table
+    df.insert(0, "incr_line_id", range(max_result+1, max_result+len(df)))
+
+    # Create staging table
+    staging_name = table_name + "_test"
+    try:
+        df.to_sql(staging_name, engine, chunksize=10000, index="incr_line_id")
+    except ValueError:
+        sys.exit(f"Could not create staging table {staging_name}")
+    print(f"Created staging table {staging_name}")
+
+    # Dropping staging table
+    staging_table = sql.Identifier(staging_name)
+    cursor.execute(sql.SQL(f"DROP TABLE {staging_table};"))
+
+    # Appending to table
+    df.to_sql(table_name, engine, if_exists="append", index=False)
+    connection.commit()
 
 
 def main():
-    conn = psycopg2.connect(database=DB, user=UN, password=PW, host=H, port=PORT)
+    db = settings.DB_CREDENTIALS
+    conn = psycopg2.connect(database=db["db_name"], user=db["un"], password=db["pw"], host=db["host"], port=db["port"])
     cur = conn.cursor()
-    engine = create_engine("postgresql://" + UN + ":" + PW + "@" + H + ":" + PORT + "/" + DB, echo=True)
+    engine = create_engine("postgresql://" + db["un"] + ":" + db["pw"] + "@" + db["host"] + ":" + db["port"] + "/" +
+                           db["db_name"], echo=True)
     print("Engines created.")
 
-    files = sorted(glob.glob(READ_PATH + "*.csv"))
+    files = sorted(glob.glob(settings.READ_PATH + "*.csv"))
     for f in files:
         table_name = f.rpartition('_')[0].rpartition('/')[2]
         print(table_name)
@@ -48,7 +74,7 @@ def main():
         parse_setting = False if table_name in ["competitors", "judges"] else ["event_start_date"]
         infer_setting = False if table_name in ["competitors", "judges"] else True
 
-        data = pd.read_csv(f, na_values='', low_memory=False, parse_dates=parse_setting,
+        data = pd.read_csv(f, na_values="", low_memory=False, parse_dates=parse_setting,
                            infer_datetime_format=infer_setting)
         upload_new_table(cur, conn, engine, data, table_name)
 
